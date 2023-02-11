3526fc4a1820ddf547a7057bdb763b5c7219fbb1
Keith Stanley <iamstan13y@gmail.com>
2023-02-11 22:34:26 +0200

    DB Access class.

---
+++ LMS/LMS/Utility/Db.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.SQLite;
+
+namespace LMS.Utility
+{
+    public class Db
+    {
+        public static readonly SQLiteConnection Connection = new("Data Source=./LMS.db");
+        public static readonly SQLiteCommand? Command;
+        public static readonly SQLiteDataReader? DataReader;
+        public static readonly SQLiteDataAdapter? Adapter;
+    }
+}

faaf29d31d1d8d312530b2468fcba3fa943231d8
Keith Stanley <iamstan13y@gmail.com>
2023-02-12 23:13:09 +0200

    Dashboard minor fixes.

--- LMS/LMS/Dashboard.Designer.cs
+++ LMS/LMS/Dashboard.Designer.cs
@@ -28,12 +28,115 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblTotalBooks = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lblBorrowedBooks = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.lblLostBooks = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.GhostWhite;
+            this.groupBox1.Controls.Add(this.lblTotalBooks);
+            this.groupBox1.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.groupBox1.Location = new System.Drawing.Point(23, 35);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(218, 99);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Total Books in Library";
+            // 
+            // lblTotalBooks
+            // 
+            this.lblTotalBooks.AutoSize = true;
+            this.lblTotalBooks.Font = new System.Drawing.Font("Lucida Sans", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblTotalBooks.ForeColor = System.Drawing.Color.DimGray;
+            this.lblTotalBooks.Location = new System.Drawing.Point(66, 36);
+            this.lblTotalBooks.Name = "lblTotalBooks";
+            this.lblTotalBooks.Size = new System.Drawing.Size(77, 37);
+            this.lblTotalBooks.TabIndex = 0;
+            this.lblTotalBooks.Text = "500";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.GhostWhite;
+            this.groupBox2.Controls.Add(this.lblBorrowedBooks);
+            this.groupBox2.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.groupBox2.Location = new System.Drawing.Point(292, 35);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(218, 99);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Borrowed Books";
+            // 
+            // lblBorrowedBooks
+            // 
+            this.lblBorrowedBooks.AutoSize = true;
+            this.lblBorrowedBooks.Font = new System.Drawing.Font("Lucida Sans", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblBorrowedBooks.ForeColor = System.Drawing.Color.DimGray;
+            this.lblBorrowedBooks.Location = new System.Drawing.Point(66, 36);
+            this.lblBorrowedBooks.Name = "lblBorrowedBooks";
+            this.lblBorrowedBooks.Size = new System.Drawing.Size(77, 37);
+            this.lblBorrowedBooks.TabIndex = 0;
+            this.lblBorrowedBooks.Text = "500";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.Color.GhostWhite;
+            this.groupBox3.Controls.Add(this.lblLostBooks);
+            this.groupBox3.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.groupBox3.Location = new System.Drawing.Point(566, 35);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(218, 99);
+            this.groupBox3.TabIndex = 1;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Damaged/Lost";
+            // 
+            // lblLostBooks
+            // 
+            this.lblLostBooks.AutoSize = true;
+            this.lblLostBooks.Font = new System.Drawing.Font("Lucida Sans", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblLostBooks.ForeColor = System.Drawing.Color.DimGray;
+            this.lblLostBooks.Location = new System.Drawing.Point(66, 36);
+            this.lblLostBooks.Name = "lblLostBooks";
+            this.lblLostBooks.Size = new System.Drawing.Size(77, 37);
+            this.lblLostBooks.TabIndex = 0;
+            this.lblLostBooks.Text = "500";
+            // 
+            // Dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(809, 461);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Dashboard";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Dashboard";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private GroupBox groupBox1;
+        private Label lblTotalBooks;
+        private GroupBox groupBox2;
+        private Label lblBorrowedBooks;
+        private GroupBox groupBox3;
+        private Label lblLostBooks;
     }
 }

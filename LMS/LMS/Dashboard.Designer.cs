ba00747675d6638e6c57a8b1aa04d026de2e0951
Keith Stanley <iamstan13y@gmail.com>
2023-02-12 23:40:59 +0200

    Dashboard Stuff.

--- LMS/LMS/Dashboard.Designer.cs
+++ LMS/LMS/Dashboard.Designer.cs
@@ -34,6 +34,7 @@
             this.lblBorrowedBooks = new System.Windows.Forms.Label();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.lblLostBooks = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
@@ -44,7 +45,7 @@
             this.groupBox1.BackColor = System.Drawing.Color.GhostWhite;
             this.groupBox1.Controls.Add(this.lblTotalBooks);
             this.groupBox1.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.groupBox1.Location = new System.Drawing.Point(23, 35);
+            this.groupBox1.Location = new System.Drawing.Point(22, 78);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(218, 99);
             this.groupBox1.TabIndex = 0;
@@ -56,18 +57,18 @@
             this.lblTotalBooks.AutoSize = true;
             this.lblTotalBooks.Font = new System.Drawing.Font("Lucida Sans", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.lblTotalBooks.ForeColor = System.Drawing.Color.DimGray;
-            this.lblTotalBooks.Location = new System.Drawing.Point(66, 36);
+            this.lblTotalBooks.Location = new System.Drawing.Point(86, 36);
             this.lblTotalBooks.Name = "lblTotalBooks";
-            this.lblTotalBooks.Size = new System.Drawing.Size(77, 37);
+            this.lblTotalBooks.Size = new System.Drawing.Size(37, 37);
             this.lblTotalBooks.TabIndex = 0;
-            this.lblTotalBooks.Text = "500";
+            this.lblTotalBooks.Text = "--";
             // 
             // groupBox2
             // 
             this.groupBox2.BackColor = System.Drawing.Color.GhostWhite;
             this.groupBox2.Controls.Add(this.lblBorrowedBooks);
             this.groupBox2.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.groupBox2.Location = new System.Drawing.Point(292, 35);
+            this.groupBox2.Location = new System.Drawing.Point(291, 78);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Size = new System.Drawing.Size(218, 99);
             this.groupBox2.TabIndex = 1;
@@ -79,18 +80,18 @@
             this.lblBorrowedBooks.AutoSize = true;
             this.lblBorrowedBooks.Font = new System.Drawing.Font("Lucida Sans", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.lblBorrowedBooks.ForeColor = System.Drawing.Color.DimGray;
-            this.lblBorrowedBooks.Location = new System.Drawing.Point(66, 36);
+            this.lblBorrowedBooks.Location = new System.Drawing.Point(86, 36);
             this.lblBorrowedBooks.Name = "lblBorrowedBooks";
-            this.lblBorrowedBooks.Size = new System.Drawing.Size(77, 37);
+            this.lblBorrowedBooks.Size = new System.Drawing.Size(37, 37);
             this.lblBorrowedBooks.TabIndex = 0;
-            this.lblBorrowedBooks.Text = "500";
+            this.lblBorrowedBooks.Text = "--";
             // 
             // groupBox3
             // 
             this.groupBox3.BackColor = System.Drawing.Color.GhostWhite;
             this.groupBox3.Controls.Add(this.lblLostBooks);
             this.groupBox3.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.groupBox3.Location = new System.Drawing.Point(566, 35);
+            this.groupBox3.Location = new System.Drawing.Point(565, 78);
             this.groupBox3.Name = "groupBox3";
             this.groupBox3.Size = new System.Drawing.Size(218, 99);
             this.groupBox3.TabIndex = 1;
@@ -102,17 +103,30 @@
             this.lblLostBooks.AutoSize = true;
             this.lblLostBooks.Font = new System.Drawing.Font("Lucida Sans", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.lblLostBooks.ForeColor = System.Drawing.Color.DimGray;
-            this.lblLostBooks.Location = new System.Drawing.Point(66, 36);
+            this.lblLostBooks.Location = new System.Drawing.Point(86, 36);
             this.lblLostBooks.Name = "lblLostBooks";
-            this.lblLostBooks.Size = new System.Drawing.Size(77, 37);
+            this.lblLostBooks.Size = new System.Drawing.Size(37, 37);
             this.lblLostBooks.TabIndex = 0;
-            this.lblLostBooks.Text = "500";
+            this.lblLostBooks.Text = "--";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label1.Location = new System.Drawing.Point(279, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(249, 32);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Admin Dashboard";
             // 
             // Dashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(809, 461);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
@@ -120,6 +134,7 @@
             this.Name = "Dashboard";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Dashboard";
+            this.Load += new System.EventHandler(this.Dashboard_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
@@ -127,6 +142,7 @@
             this.groupBox3.ResumeLayout(false);
             this.groupBox3.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -138,5 +154,6 @@
         private Label lblBorrowedBooks;
         private GroupBox groupBox3;
         private Label lblLostBooks;
+        private Label label1;
     }
 }

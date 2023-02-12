f7104653279c3305c931894958c68833a278d667
Keith Stanley <iamstan13y@gmail.com>
2023-02-12 21:41:59 +0200

    Login WIP.

--- LMS/LMS/LoginForm.cs
+++ LMS/LMS/LoginForm.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using LMS.Utility;
+using System.Data.SQLite;
 
 namespace LMS
 {
@@ -15,6 +8,42 @@ namespace LMS
         public LoginForm()
         {
             InitializeComponent();
+        }
+
+        private void BtnContinue_Click(object sender, EventArgs e)
+        {
+            if (Login(txtUsername.Text, txtPassword.Text))
+            {
+
+            }
+            else
+            {
+                MessageBox.Show("Wrong user credentials!", "Login error", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+            }
+        }
+
+        private bool Login(string Username, string Password)
+        {
+            try
+            {
+
+                Db.Connection.Open();
+                Db.Command = new SQLiteCommand("SELECT * FROM [Users] WHERE [Username]='" + Username + "' AND [Password]='" + Password + "'", Db.Connection);
+                Db.DataReader = Db.Command.ExecuteReader();
+
+                if (Db.DataReader.Read())
+                    return true;
+                return false;
+            }
+            catch
+            {
+                return false;
+            }
+            finally
+            {
+                Db.Connection.Close();
+                Db.Command!.Dispose();
+            }
         }
     }
 }

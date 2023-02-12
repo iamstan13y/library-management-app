8eda4a6aa3e097963f246b8fd5d64685f92e3c1e
Keith Stanley <iamstan13y@gmail.com>
2023-02-12 23:13:05 +0200

    Dashboard minor fixes.

--- LMS/LMS/Dashboard.cs
+++ LMS/LMS/Dashboard.cs
@@ -1,20 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace LMS
+﻿namespace LMS
 {
     public partial class Dashboard : Form
     {
         public Dashboard()
         {
             InitializeComponent();
+        }
+
+        private void Dashboard_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }

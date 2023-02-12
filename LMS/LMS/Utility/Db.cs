4d32ebbc162c9692cfe35444fcd0716b55e8c81e
Keith Stanley <iamstan13y@gmail.com>
2023-02-12 21:41:57 +0200

    Login WIP.

--- LMS/LMS/Utility/Db.cs
+++ LMS/LMS/Utility/Db.cs
@@ -5,8 +5,8 @@ namespace LMS.Utility
     public class Db
     {
         public static readonly SQLiteConnection Connection = new("Data Source=./LMS.db");
-        public static readonly SQLiteCommand? Command;
-        public static readonly SQLiteDataReader? DataReader;
-        public static readonly SQLiteDataAdapter? Adapter;
+        public static SQLiteCommand? Command;
+        public static SQLiteDataReader? DataReader;
+        public static SQLiteDataAdapter? Adapter;
     }
 }
